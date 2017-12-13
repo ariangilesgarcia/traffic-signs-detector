a8c4d6bb1c316034d3c2f97a2382710fb7d5b8e4
Arian Giles García <arian2822@gmail.com>
2017-12-13 15:32:26 -0300

    Initial commit

---
+++ detector/cropper.py
@@ -0,0 +1,51 @@
+
+
+class Cropper:
+
+    def __init__(self, crop_percent, force_square=False):
+        self.crop_percent = crop_percent
+        self.force_square = force_square
+
+
+    def expand_coordinates(self, original_coordinates, image_size):
+        # Get separate values
+        x1, y1, x2, y2 = original_coordinates
+        img_width, img_height = image_size
+
+        # Calculate width and height of crop
+        width = x2 - x1
+        height = y2 - y1
+
+        # Calculate center point of crop
+        center_x = x1 + int(width/2)
+        center_y = y1 + int(height/2)
+
+        # Calculate the size of the square side
+        size = max(width, height)
+
+        # Force square if parameter is set
+        if self.force_square:
+            size = int(size/2 * (1 + self.crop_percent))
+
+        # Calculate new coords
+        x1 = center_x - size
+        x2 = center_x + size
+        y1 = center_y - size
+        y2 = center_y + size
+
+        if x1 < 0:
+            x1 = 0
+        if x2 > img_width:
+            x2 = img_width
+        if y1 < 0:
+            y1 = 0
+        if y2 > img_height:
+            y2 = img_height
+
+        new_coords = x1, y1, x2, y2
+
+        return new_coords
+
+
+    def crop_image(self):
+        pass
---
+++ tests/test_cropper.py
@@ -0,0 +1,229 @@
+import os
+import sys
+import unittest
+
+from .context import detector
+from detector.cropper import Cropper
+
+
+class TestCropper:
+
+
+    def test_expand_coordinates_inside(self):
+        crop_percent = 0.1
+        force_square = False
+
+        image_size = (1920, 1080)
+
+        x1 = 500
+        y1 = 300
+
+        width = 100
+        height = 200
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (350, 200, 750, 600)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_right_side(self):
+        crop_percent = 0.1
+        force_square = False
+
+        image_size = (600, 800)
+
+        x1 = 500
+        y1 = 300
+
+        width = 100
+        height = 200
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (350, 200, 600, 600)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_left_side(self):
+        crop_percent = 0.1
+        force_square = False
+
+        image_size = (1920, 1080)
+
+        x1 = 0
+        y1 = 500
+
+        width = 100
+        height = 200
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (0, 400, 250, 800)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_top_side(self):
+        crop_percent = 0.1
+        force_square = False
+
+        image_size = (1920, 1080)
+
+        x1 = 500
+        y1 = 0
+
+        width = 200
+        height = 100
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (400, 0, 800, 250)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_bottom_side(self):
+        crop_percent = 0.1
+        force_square = False
+
+        image_size = (1920, 1080)
+
+        x1 = 500
+        y1 = 1000
+
+        width = 100
+        height = 80
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (450, 940, 650, 1080)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_square_inside(self):
+        crop_percent = 0.1
+        force_square = True
+
+        image_size = (1920, 1080)
+
+        x1 = 500
+        y1 = 300
+
+        width = 100
+        height = 200
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (440, 290, 660, 510)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_square_right_side(self):
+        crop_percent = 0.1
+        force_square = True
+
+        image_size = (600, 800)
+
+        x1 = 500
+        y1 = 300
+
+        width = 100
+        height = 200
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (440, 290, 600, 510)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_square_left_side(self):
+        crop_percent = 0.1
+        force_square = True
+
+        image_size = (1920, 1080)
+
+        x1 = 0
+        y1 = 500
+
+        width = 100
+        height = 200
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (0, 490, 160, 710)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_square_top_side(self):
+        crop_percent = 0.1
+        force_square = True
+
+        image_size = (1920, 1080)
+
+        x1 = 500
+        y1 = 0
+
+        width = 200
+        height = 100
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (490, 0, 710, 160)
+
+        assert expanded_coordinates == expected_coordinates
+
+
+    def test_expand_coordinates_square_bottom_side(self):
+        crop_percent = 0.1
+        force_square = True
+
+        image_size = (1920, 1080)
+
+        x1 = 500
+        y1 = 1000
+
+        width = 100
+        height = 80
+
+        original_coordinates = (x1, y1, x1 + width, y1 + height)
+
+        crp = Cropper(crop_percent, force_square)
+
+        expanded_coordinates =  crp.expand_coordinates(original_coordinates, image_size)
+        expected_coordinates = (495, 985, 605, 1080)
+
+        assert expanded_coordinates == expected_coordinates
