3df674ae2c01fb698b26bdeffe020ef597fa59c6
Arian Giles Garcia <arian2822@gmail.com>
2017-12-14 17:50:16 -0300

    Added logger class

---
+++ detector/logger.py
@@ -0,0 +1,25 @@
+import json
+
+from .utils import convert_detection_to_csv
+from .utils import convert_detection_to_yolo
+
+
+class Logger:
+
+    def save_detections_to_csv(self, detections, output):
+        with open(output, 'w') as fp:
+            for detection in detections:
+                csv_detection = convert_detection_to_csv(detection)
+                fp.write(csv_detection + '\n')
+
+
+    def save_detections_to_json(self, detections, output):
+        with open(output, 'w') as fp:
+            json.dump(detections, fp)
+
+
+    def save_detections_to_yolo(self, detections, image_size, output):
+        with open(output, 'w') as fp:
+            for detection in detections:
+                yolo_detection = convert_detection_to_yolo(detection, image_size)
+                fp.write(yolo_detection + '\n')
