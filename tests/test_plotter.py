8759d4985b05e9818b676edbd667818067aa316c
Arian Giles García <arian2822@gmail.com>
2017-12-14 12:03:43 -0300

    Added tests for plotter coverage

--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -36,3 +36,87 @@ class TestPlotter:
         drawn_image = plt.plot_detections(image, test_detections)
 
         assert drawn_image.shape == image.shape
+
+
+    def test_plot_detections_bgr(self):
+        plt = Plotter(num_classes=20, bgr=True)
+
+        test_detections = [
+            {
+                'coordinates': [200, 100, 300, 200],
+                'confidence': 0.998,
+                'class_id': 16,
+                'label': 'contramano'
+            },
+            {
+                'coordinates': [300, 800, 400, 950],
+                'confidence': 0.783,
+                'class_id': 13,
+                'label': 'cocoa'
+            }
+        ]
+
+        test_image_url = 'https://www.pyimagesearch.com/wp-content/uploads/2015/01/opencv_logo.png'
+        response = urllib.request.urlopen(test_image_url)
+        image = np.asarray(bytearray(response.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        drawn_image = plt.plot_detections(image, test_detections)
+
+        assert drawn_image.shape == image.shape
+
+
+    def test_plot_detections_confidence(self):
+        plt = Plotter(num_classes=20)
+
+        test_detections = [
+            {
+                'coordinates': [200, 100, 300, 200],
+                'confidence': 0.998,
+                'class_id': 16,
+                'label': 'contramano'
+            },
+            {
+                'coordinates': [300, 800, 400, 950],
+                'confidence': 0.783,
+                'class_id': 13,
+                'label': 'cocoa'
+            }
+        ]
+
+        test_image_url = 'https://www.pyimagesearch.com/wp-content/uploads/2015/01/opencv_logo.png'
+        response = urllib.request.urlopen(test_image_url)
+        image = np.asarray(bytearray(response.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        drawn_image = plt.plot_detections(image, test_detections, draw_confidence=True)
+
+        assert drawn_image.shape == image.shape
+
+
+    def test_plot_detections_right_side(self):
+        plt = Plotter(num_classes=20)
+
+        test_detections = [
+            {
+                'coordinates': [450, 400, 500, 500],
+                'confidence': 0.998,
+                'class_id': 16,
+                'label': 'contramano'
+            },
+            {
+                'coordinates': [300, 800, 400, 950],
+                'confidence': 0.783,
+                'class_id': 13,
+                'label': 'cocoa'
+            }
+        ]
+
+        test_image_url = 'https://www.pyimagesearch.com/wp-content/uploads/2015/01/opencv_logo.png'
+        response = urllib.request.urlopen(test_image_url)
+        image = np.asarray(bytearray(response.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        drawn_image = plt.plot_detections(image, test_detections)
+
+        assert drawn_image.shape == image.shape
