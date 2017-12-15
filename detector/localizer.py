d53aadb3cd9119659eeb2bb1c92602032463aeed
Arian Giles Garcia <arian2822@gmail.com>
2017-12-15 14:48:34 -0300

    Added Localizer class (yolo moddel)

---
+++ detector/localizer.py
@@ -0,0 +1,37 @@
+from darkflow.net.build import TFNet
+
+
+class Localizer:
+
+    def __init__(self, cfg_path, weights_path, threshold):
+        options = {
+            "model": cfg_path,
+            "load": weights_path,
+            "threshold": threshold,
+        }
+
+        self.localizer_model = TFNet(options)
+
+
+    def find_objects_in_image(self, image):
+        detections = self.localizer_model.return_predict(image)
+
+        new_detections = []
+
+        for detection in detections:
+            new_detection = {}
+
+            x1 = detection['topleft']['x']
+            y1 = detection['topleft']['y']
+            x2 = detection['bottomright']['x']
+            y2 = detection['bottomright']['y']
+
+            new_detection['coordinates'] = [x1, y1, x2, y2]
+            new_detection['confidence'] = detection['confidence']
+            new_detection['label'] = detection['label']
+            new_detection['class_id'] = 0
+
+
+            new_detections.append(new_detection)
+
+        return new_detections
