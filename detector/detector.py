f12bf9391c8a342547bc4098aa9954e43517c536
Arian Giles Garcia <arian2822@gmail.com>
2017-12-16 17:10:45 -0300

    Added first version of detector

---
+++ detector/detector.py
@@ -0,0 +1,25 @@
+
+
+class Detector:
+
+    def __init__(self, localizer, cropper, classifier):
+        self.localizer = localizer
+        self.cropper = cropper
+        self.classifier = classifier
+
+
+    def detect_objects_in_image(self, image):
+        object_locations = self.localizer.find_objects_in_image(image)
+
+        classified_detections = []
+
+        for location in object_locations:
+            coordinates = location['coordinates']
+            cropped_image = self.cropper.expand_and_crop(image, coordinates)
+            classified_object = self.classifier.classify_image(cropped_image)
+
+            if classified_object:
+                classified_object['coordinates'] = coordinates
+                classified_detections.append(classified_object)
+
+        return classified_detections
