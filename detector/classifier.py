c5eb0b14f8b9fa5b79100cf047ed3db1a75c35a3
Arian Giles Garcia <arian2822@gmail.com>
2017-12-16 15:32:20 -0300

    Added classifier class

---
+++ detector/classifier.py
@@ -0,0 +1,51 @@
+import cv2
+import keras
+import numpy as np
+
+class Classifier:
+
+    def __init__(self, model_path, weights_path, labels_path, threshold):
+        with open(model_path, 'r') as json_file:
+            model_json = json_file.read()
+
+        self.model = keras.models.model_from_json(model_json)
+        self.model.load_weights(weights_path)
+
+        self.threshold = threshold
+
+        labels = open(labels_path, 'r').read().strip().split('\n')
+
+        self.class_map = {}
+        class_id = 0
+
+        for label in labels:
+            self.class_map[class_id] = label
+            class_id += 1
+
+        self.input_size = tuple(self.model.layers[0].get_output_at(0).get_shape().as_list()[1:-1])
+
+
+    def classify_image(self, image):
+        # Resize image
+        image = image[..., ::-1]
+        image = cv2.resize(image, self.input_size)
+
+        # Add batch dimension
+        image = np.expand_dims(image, axis=0)
+
+        # Predict class for image
+        predictions = self.model.predict(image, verbose=False)[0]
+        class_id = np.argmax(predictions)
+
+        prediction = None
+
+        # If the prediction has greater confidence than the threshold
+        if predictions[class_id] > self.threshold:
+            # Populate the prediction object
+            prediction = {
+                'class_id': class_id,
+                'label': self.class_map[class_id],
+                'confidence': predictions[class_id],
+            }
+
+        return prediction
