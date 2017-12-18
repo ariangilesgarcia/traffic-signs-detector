b79ff23e7332ece2e9fb8bbd04b345c3e85547ab
Arian Giles Garcia <arian2822@gmail.com>
2017-12-17 21:16:03 -0300

    Added screen management

--- gui/main/app.py
+++ gui/main/app.py
@@ -1,20 +1,39 @@
 import kivy
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.config import Config
-from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 
-class DetectorLayout(BoxLayout):
+# Define app screens
+class MainScreen(Screen):
     pass
 
 
+class DetectImageScreen(Screen):
+    pass
+
+
+# Define screen manager
+class ScreenManagement(ScreenManager):
+    pass
+
+
+# Load .kv file
+ui = Builder.load_file('detector.kv')
+
+
+# DetectorApp Class
 class DetectorApp(App):
 
     def build(self):
         Config.set('graphics', 'fullscreen', 'auto')
-        return DetectorLayout()
+        return ui
 
 
+# Create and run app
+Window.fullscreen = 'auto'
 app = DetectorApp()
 app.run()
