b464b1d2f6305216dcad89cbaa0989690fa2fee3
Arian Giles Garcia <arian2822@gmail.com>
2017-12-18 00:11:51 -0300

    Image results progress

--- gui/main/app.py
+++ gui/main/app.py
@@ -1,10 +1,18 @@
-import kivy
+import cv2
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.core.window import Window
+from kivy.event import EventDispatcher
+from kivy.graphics.texture import Texture
+from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
+
+# Data manager
+class DataManager(EventDispatcher):
+    image_path = StringProperty()
 
 
 # Define app screens
@@ -13,12 +21,30 @@ class MainScreen(Screen):
 
 
 class DetectImageScreen(Screen):
-    pass
+
+    def save_image_path(self):
+        self.manager.state_data.image_path = self.ids.filechooser.selection[0]
+
+
+class ImageResultScreen(Screen):
+
+    def on_enter(self):
+        img = cv2.imread(self.manager.state_data.image_path)
+        buf1 = cv2.flip(img, 0)
+        buf = buf1.tostring()
+        image_texture = Texture.create(size=(img.shape[1], img.shape[0]), colorfmt='bgr')
+        image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+
+        self.ids.image_results.texture = image_texture
+
+    def remove_picture(self):
+        self.ids.image_results.texture = Texture.create(size=(1,1))
 
 
 # Define screen manager
 class ScreenManagement(ScreenManager):
-    pass
+    state_data = ObjectProperty(DataManager())
+
 
 
 # Load .kv file
