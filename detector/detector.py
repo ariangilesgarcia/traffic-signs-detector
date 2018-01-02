eeba1f0500797e6035284ba186c3e42ac5d7ef66
Arian Giles Garcia <arian2822@gmail.com>
2018-01-02 14:21:04 -0300

    Added CFD and video detection

--- detector/detector.py
+++ detector/detector.py
@@ -1,30 +1,31 @@
 import os
 import cv2
+import glob
 import json
+import numpy as np
 
+from logger import Logger
+from plotter import Plotter
 from cropper import Cropper
 from localizer import Localizer
 from classifier import Classifier
 from detection_pipeline import DetectionPipeline
-from plotter import Plotter
-from logger import Logger
+from cross_frame_detector import CrossFrameDetector
+
+from exceptions import FormatNotSupportedException
 
 
 class Detector:
 
-    def __init__(self):
-        # Create objects
-        cropper = Cropper(0.25, force_square=True)
-        localizer = Localizer(cfg_path='../data/yolo/full/trafficsigns.cfg',
-                              weights_path='../data/yolo/full/trafficsigns.weights',
-                              threshold=0.1)
-        classifier = Classifier(model_path='../data/classifier/trafficsigns.json',
-                                weights_path='../data/classifier/trafficsigns.h5',
-                                labels_path='../data/classifier/classes.txt',
-                                threshold=0.9)
-        self.detection_pipeline = DetectionPipeline(localizer, cropper, classifier)
+    def __init__(self, detection_pipeline):
+        self.detection_pipeline = detection_pipeline
         self.plotter = Plotter(num_classes=20, bgr=True)
         self.logger = Logger()
+        self.cfd = CrossFrameDetector(num_classes=20,
+                                      frame_history_count=5,
+                                      frames_threshold=2)
+
+        self.classes_images = self.load_classes_images('../data/classifier/classes')
 
 
     def detect_image(self,
@@ -46,11 +47,96 @@ class Detector:
             elif extension == '.jpg':
                 detections_image = self.plotter.plot_detections(image, detections)
                 cv2.imwrite(output, detections_image)
+            else:
+                raise FormatNotSupportedException('Output extension must be .json .txt .csv or .jpg')
 
         return detections
 
 
+    def detect_video(self,
+                     video_path,
+                     show_output=False,
+                     output=None):
+
+        cap = cv2.VideoCapture(video_path)
+
+        if show_output:
+            from screeninfo import get_monitors
+            monitor = get_monitors()[0]
+            screen_w, screen_h, = monitor.width, monitor.height
+
+            window_name = 'Detector'
+            cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+            cv2.moveWindow(window_name, screen_w - 1, screen_h - 1)
+            cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            detections = self.detection_pipeline.detect_objects_in_image(frame)
+            self.cfd.register_detections(detections)
+
+            frame = self.plotter.plot_detections(frame, detections)
+
+            cfd_detections = self.cfd.get_detections()
+            cfd_detection_count = len(cfd_detections)
+
+            if cfd_detection_count > 0:
+                aux = np.zeros(shape=(250, 250*cfd_detection_count, 3), dtype='uint8')
+                start_h, start_w = 0, 0
+
+                for class_id in cfd_detections:
+                    img = self.classes_images[class_id]
+
+                    end_w = start_w + img.shape[1]
+                    aux[start_h:img.shape[0], start_w:end_w] = img
+                    start_w = start_w + img.shape[1]
+
+                frame_height, frame_width, _ = frame.shape
+                frame[frame_height-250:, :250*cfd_detection_count] = aux
+
+            # Display the resulting frame
+            if show_output:
+                cv2.imshow(window_name, frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+    def load_classes_images(self, path):
+        classes_images = {}
+
+        images_list = glob.glob(os.path.join(path, '*.jpg'))
+
+        for image_path in images_list:
+            class_id = int(os.path.basename(image_path)[:-4])
+            image = cv2.imread(image_path)
+
+            classes_images[class_id] = image
+
+        return classes_images
+
+
 if __name__ == '__main__':
-    detector = Detector()
-    img = cv2.imread('/home/arian/fortnite.png')
-    detections = detector.detect_image(img, output='/home/arian/predictions.jpg')
+    cropper = Cropper(0.25, force_square=True)
+    localizer = Localizer(cfg_path='../data/yolo/full/trafficsigns.cfg',
+                          weights_path='../data/yolo/full/trafficsigns.weights',
+                          threshold=0.1)
+    classifier = Classifier(model_path='../data/classifier/trafficsigns.json',
+                            weights_path='../data/classifier/trafficsigns.h5',
+                            labels_path='../data/classifier/classes.txt',
+                            threshold=0.9)
+    detection_pipeline = DetectionPipeline(localizer, cropper, classifier)
+
+    detector = Detector(detection_pipeline)
+
+    video_path = '/home/arian/ruta.mp4'
+    detections = detector.detect_video(video_path, show_output=True)
