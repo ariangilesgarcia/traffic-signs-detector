f18ed39171d963b83bb0b7d9954008dcd11500d7
Arian Giles García <arian2822@gmail.com>
2018-02-03 17:59:44 -0300

    Added detector tests

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -206,6 +206,49 @@ class TestDetector:
         assert os.path.exists(output_filename) == True
 
 
+    def test_detector_video(self):
+        # Create Localizer
+        cfg_path = './tests/data/trafficsigns.cfg'
+        weights_path = './tests/data/trafficsigns.weights'
+        threshold = 0.24
+
+        localizer = Localizer(cfg_path, weights_path, threshold, gpu=0.0)
+
+        # Create cropper
+        crop_percent = 0.25
+        force_square = True
+
+        cropper = Cropper(crop_percent, force_square)
+
+        # Create classifier
+        model_path = './tests/data/trafficsigns.json'
+        weights_path = './tests/data/trafficsigns.h5'
+        labels_path = './tests/data/classes.txt'
+        threshold = 0.5
+
+        classifier = Classifier(model_path, weights_path, labels_path, threshold)
+
+        # Create detection pipeline
+        detection_pipeline = DetectionPipeline(localizer, cropper, classifier)
+
+        # Create detector
+        detector = Detector(detection_pipeline)
+
+        # Detect on video
+        video_feed = './tests/data/test.mp4'
+
+        output_mp4 = './tests/data/test_output.mp4'
+        output_csv = './tests/data/test_output.csv'
+
+        exit_code = detector.detect_video_feed(video_feed,
+                                               output=output_mp4,
+                                               output_csv=output_csv,
+                                               show_confidence=True,
+                                               sound_notifications=True)
+
+        assert exit_code == True
+
+
     def test_detector_from_file(self):
         # Create detector
         detector = create_detector_from_file('./tests/data/config.json')
